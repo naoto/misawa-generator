97a35628381d6efb26d8a9f491b2d9d2fc462b63
Naoto SHINGAKI <nao.sora+github@gmail.com>
2012-06-20 12:48:50 +0900

    Oops

--- web.rb
+++ web.rb
@@ -13,12 +13,18 @@ get '/' do
 end
 
 get '/write' do
-  @name = CGI.escapeHTML(params[:user_id])
-  @image_path = MisawaMagick.new(params[:user_id]).create
-  erb :generate
+  name = CGI.escapeHTML(params[:user_id])
+  tweet_id = Twitter.new(params[:user_id]).random.tweet
+  redirect "/#{name}/#{tweet_id}"
 end
 
 get '/image/:id' do
   content_type "image/jpeg"
   File.open("/tmp/#{params[:id]}", 'rb') { |f| f.read }
+end
+
+get '/:user/:id' do
+  @name = CGI.escapeHTML(params[:user])
+  @image_path = MisawaMagick.new(params[:id]).create
+  erb :generate
 end
